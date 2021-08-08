3362170f4095c773ddf3372c836cace9aa5642e4
opdshe <opdshe@naver.com>
2021-08-08 16:39:49 +0900

    [#1] test: 데이터 전송 부분 테스트 코드 작성

--- log-manager-producer/src/test/java/com/americano/logmanager/LogMessageProducerTest.java
+++ log-manager-producer/src/test/java/com/americano/logmanager/LogMessageProducerTest.java
@@ -6,23 +6,32 @@ import com.americano.logmanager.gateway.request.error.ErrorGatewayConfig;
 import com.americano.logmanager.gateway.request.error.FileWritingPolicy;
 import com.americano.logmanager.gateway.request.error.RequestErrorHandlePolicy;
 import com.americano.logmanager.gateway.request.error.RequestErrorHandler;
+import com.americano.logmanager.gateway.request.success.RequestSuccessHandlePolicy;
+import com.americano.logmanager.gateway.request.success.RequestSuccessHandler;
+import com.americano.logmanager.gateway.request.success.SuccessGatewayConfig;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.config.EnableIntegration;
-import org.springframework.test.context.event.RecordApplicationEvents;
+import org.springframework.integration.kafka.outbound.KafkaProducerMessageHandler;
+
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 
 @EnableIntegration
-@RecordApplicationEvents
 @IntegrationComponentScan(basePackages = "com.americano.logmanager")
 @SpringBootTest(classes = {RequestGateway.class, LogMessageProducer.class, RequestGatewayConfig.class
-		, ErrorGatewayConfig.class, RequestErrorHandlePolicy.class, RequestErrorHandler.class, FileWritingPolicy.class},
+		, ErrorGatewayConfig.class, RequestErrorHandlePolicy.class, RequestErrorHandler.class, FileWritingPolicy.class,
+		SuccessGatewayConfig.class, RequestSuccessHandler.class, RequestSuccessHandlePolicy.class},
 		properties = "application.properties")
 public class LogMessageProducerTest {
+	private static final String TEST_TOPIC = "test";
 
 	@Autowired
 	private LogMessageProducer logMessageProducer;
@@ -30,20 +39,52 @@ public class LogMessageProducerTest {
 	@Autowired
 	private RequestGateway requestGateway;
 
-	@Autowired
+	@MockBean
 	private RequestErrorHandler errorHandler;
+
+	@MockBean
+	private RequestSuccessHandler successHandler;
+
+	@MockBean
+	private KafkaProducerMessageHandler<String, String> kafkaProducerMessageHandler;
+
+	/**
+	 * 테스트 실행 시 application.properties에 적절한 bootstrap-server 입력해두어야함
+	 */
+
+	@Test
+	void 데이터_전송_성공시_에러_대응로직_실행되면_안됨() {
+		//given
+		DummyObject dummyObject = new DummyObject("dummy");
+
+		//when
+		logMessageProducer.send(TEST_TOPIC, dummyObject);
+
+		//then
+		Mockito.verify(errorHandler, Mockito.never()).handleMessage(any());
+	}
+
+	@Test
+	void 데이터_전송중_예외발생시_대응로직_실행되어야함() throws ExecutionException, InterruptedException {
+		//given
+		DummyObject dummyObject = new DummyObject("dummy");
+
+		//mocking
+		Mockito.doThrow(RuntimeException.class).when(kafkaProducerMessageHandler).processSendResult(any(), any(), any(), any());
+
+		//when
+		logMessageProducer.send(TEST_TOPIC, dummyObject);
+
+		//then
+		Mockito.verify(errorHandler, Mockito.atLeastOnce()).handleMessage(any());
+		Mockito.verify(successHandler, Mockito.never()).handleMessage(any());
+	}
 
 	@Test
 	void topic_혹은_value가_null_이면_예외_발생해야_함() {
 		//when & then
 		assertThrows(IllegalArgumentException.class, () -> logMessageProducer.send(null, "not null"));
 		assertThrows(IllegalArgumentException.class, () -> logMessageProducer.send("not null", null));
-	}
-
-	@Test
-	void test() {
-		logMessageProducer.send("test", new DummyObject("test object"));
-
 	}
 
 	private static class DummyObject {
